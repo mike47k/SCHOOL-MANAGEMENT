ebe2fa312072387c10925705ccdbe8fdde6c117e
Gabriel <gam98lhda@gmail.com>
2021-07-16 17:21:30 -0300

    Modal working!

---
+++ School_Managment3/src/main/resources/static/js/main.js
@@ -0,0 +1,27 @@
+const openEls = document.querySelectorAll("[data-open]");
+const closeEls = document.querySelectorAll("[data-close]");
+const cancelEls = document.querySelectorAll("[data-cancel]");
+
+for (const el of openEls) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    //console.log(modalId)
+    document.getElementById(modalId).classList.add('modal--show');
+  });
+}
+
+for (const el of closeEls) {
+  el.addEventListener("click", function() {	
+    this.parentElement.parentElement.parentElement.classList.remove('modal--show');
+	//console.log(this.parentElement.parentElement.parentElement);
+  });
+}
+
+for (const el of cancelEls) {
+  el.addEventListener("click", function(e) {	
+	e.preventDefault();
+    this.parentElement.parentElement.parentElement.parentElement.classList.remove('modal--show');
+	//console.log(this.parentElement.parentElement.parentElement.parentElement);
+  });
+}
+
