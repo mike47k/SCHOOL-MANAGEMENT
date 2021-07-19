6aa3bdc5474b57e616b97ab04adc12ed2d538ae2
gam98 <gam98lhda@gmail.com>
2021-07-19 17:12:13 -0300

    responsive ready 🚀

--- School_Managment3/src/main/resources/static/js/app.js
+++ School_Managment3/src/main/resources/static/js/app.js
@@ -1,6 +1,35 @@
+//Menu
 const $menuIcon = document.getElementById('menu-icon')
 const $menu = document.getElementById('menu')
+//Modal
+const openEls = document.querySelectorAll("[data-open]");
+const closeEls = document.querySelectorAll("[data-close]");
+const cancelEls = document.querySelectorAll("[data-cancel]");
 
 $menuIcon.addEventListener('click',()=> {
     $menu.classList.toggle('menu--show')
 })
+
+
+for (const el of openEls) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    //console.log(modalId)
+    document.getElementById(modalId).classList.add('modal--show');
+  });
+}
+
+for (const el of closeEls) {
+  el.addEventListener("click", function() {	
+    this.parentElement.parentElement.parentElement.classList.remove('modal--show');
+	//console.log(this.parentElement.parentElement.parentElement);
+  });
+}
+
+for (const el of cancelEls) {
+  el.addEventListener("click", function(e) {	
+	e.preventDefault();
+    this.parentElement.parentElement.parentElement.parentElement.classList.remove('modal--show');
+	//console.log(this.parentElement.parentElement.parentElement.parentElement);
+  });
+}
